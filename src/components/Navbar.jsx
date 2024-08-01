3185a6a6ca49ec32e88532a92c9f5c2a6017309c
menachemlemmer <nmmlemmer@gmail.com>
2024-08-01 14:34:54 -0400

    Create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthedUserContext } from "../App";
+
+const Navbar = (props) => {
+  const user = useContext(AuthedUserContext);
+
+  return (
+    <nav className="flex items-center justify-between bg-gray-800 p-4 text-white">
+      <Link to="/" className="text-2xl font-bold">
+        SpendSense
+      </Link>
+      <div className="flex gap-4">
+        {user ? (
+          <button onClick={props.handleSignout} className="bg-none text-white">
+            Sign out
+          </button>
+        ) : (
+          <>
+            <Link to="/signin" className="text-white">
+              Log in
+            </Link>
+            <Link to="/signup" className="text-white">
+              Sign up
+            </Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
