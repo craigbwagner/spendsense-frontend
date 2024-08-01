5b8b0df8243d183928ce8aa7c22009dd41bf06a2
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-01 16:51:08 -0400

    scaffold UpdateSettings form component

---
+++ src/components/UpdateSettings.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import * as settingsService from "../services/settingsService"
+
+const getSettings = async () => {
+  const userSettings = await settingsService.index();
+
+}
+
+const formSchema = z.object({
+  monthly_income:
+});
+
+const updateSettingsForm = (props) => {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+
+    },
+  });
+
+  const handleSubmit = async (data) => {
+    try {
+      const user = await authService.signin(data);
+      props.setUser(user);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setMessage(err.message);
+    }
+    form.reset();
+  }
+  return (
+    <section className="flex">
+
+    </section>
+  )
+}
