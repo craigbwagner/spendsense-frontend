cd88cf50fefe6e02bb07e66ed4255b8ecc2fb0ec
menachemlemmer <nmmlemmer@gmail.com>
2024-08-01 13:24:25 -0400

    Create routes, fix form message, create authedUserContext, and authServices

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useState, createContext } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 import SignupForm from "./components/SignupForm";
 import SigninForm from "./components/SigninForm";
+import * as authService from "../services/authService";
+export const AuthedUserContext = createContext(null);
 
 function App() {
+  const [user, setUser] = useState(authService.getUser());
+
+  const handleSignout = () => {
+    authService.signout();
+    setUser(null);
+  };
+
   return (
     <>
-      <h1 className="text-2xl">Hello World</h1>
-      <SignupForm />
-      <SigninForm />
+      <AuthedUserContext.Provider value={user}>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+          <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+        </Routes>
+      </AuthedUserContext.Provider>
     </>
   );
 }
--- src/components/SigninForm.jsx
+++ src/components/SigninForm.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -11,25 +13,39 @@ import {
 } from "./ui/form";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import * as authService from "../../services/authService";
 
 const formSchema = z.object({
   username: z.string().min(5).max(20),
   password: z.string().min(4).max(20),
 });
 
-const SigninForm = () => {
+const SigninForm = (props) => {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: { username: "", password: "" },
   });
 
-  const handleSubmit = (data) => {
-    console.log(data);
+  const handleSubmit = async (data) => {
+    try {
+      const user = await authService.signin(data);
+      console.log(user);
+      props.setUser(user);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setMessage(err.message);
+    }
+    form.reset();
   };
 
   return (
-    <main className="m-8 flex h-full w-full items-center justify-center">
+    <main className="m-8 flex h-full w-full flex-col items-center justify-center">
       <Form {...form}>
+        {message && <p className="text-red-500">{message}</p>}
         <form
           onSubmit={form.handleSubmit(handleSubmit)}
           className="flex w-full max-w-md flex-col gap-4"
