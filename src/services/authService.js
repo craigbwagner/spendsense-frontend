44747080f4a0b6078d0b0bc79c8881b91e70c23f
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-01 14:56:40 -0400

    reconvert authService functions back to arrow functions

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,13 +1,13 @@
 const BACKEND_URL = import.meta.env.VITE_FLASK_BACKEND_URL;
 
-function getUser() {
+const getUser = () => {
   const token = localStorage.getItem("token");
   if (!token) return null;
   const user = JSON.parse(atob(token.split(".")[1]));
   return user;
 };
 
-async function signup(formData) {
+const signup = async (formData) => {
   try {
     const res = await fetch(`${BACKEND_URL}/auth/signup`, {
       method: "POST",
@@ -25,7 +25,7 @@ async function signup(formData) {
   }
 };
 
-async function signin(user) {
+const signin = async (user) => {
   try {
     const res = await fetch(`${BACKEND_URL}/auth/signin`, {
       method: "POST",
@@ -47,7 +47,7 @@ async function signin(user) {
   }
 };
 
-function signout() {
+const signout = () => {
   localStorage.removeItem("token");
 };
 
