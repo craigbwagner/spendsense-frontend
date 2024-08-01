bec238165f0d180452139420ef3d849b17104385
menachemlemmer <nmmlemmer@gmail.com>
2024-08-01 10:28:52 -0400

    Configure eslint and vite paths, and add shadcn components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-// https://vitejs.dev/config/
+function getDirname(importMetaUrl) {
+  const filename = fileURLToPath(importMetaUrl);
+  return dirname(filename);
+}
+const __dirname = getDirname(import.meta.url);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
