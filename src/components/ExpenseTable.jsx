36083f6f6500d55b3937d6426022f00a01592bfa
menachemlemmer <nmmlemmer@gmail.com>
2024-08-02 15:27:44 -0400

    Change date formatting

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -31,8 +31,12 @@ import { Label } from "./ui/label";
 const ExpenseTable = (props) => {
   const [activeTab, setActiveTab] = useState("Week");
   const [selectedCategory, setSelectedCategory] = useState("all");
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "short", day: "2-digit" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
-  const weekExpenses = props.expenses.filter((expense) => {
+  const weeklyExpenses = props.expenses.filter((expense) => {
     const expenseDate = new Date(expense.date);
     const today = new Date();
     const diff = today - expenseDate;
@@ -40,7 +44,7 @@ const ExpenseTable = (props) => {
     return days <= 7;
   });
 
-  const monthExpenses = props.expenses.filter((expense) => {
+  const monthlyExpenses = props.expenses.filter((expense) => {
     const expenseDate = new Date(expense.date);
     const today = new Date();
     const diff = today - expenseDate;
@@ -48,7 +52,7 @@ const ExpenseTable = (props) => {
     return days <= 30;
   });
 
-  const yearExpenses = props.expenses.filter((expense) => {
+  const yearlyExpenses = props.expenses.filter((expense) => {
     const expenseDate = new Date(expense.date);
     const today = new Date();
     const diff = today - expenseDate;
@@ -56,19 +60,44 @@ const ExpenseTable = (props) => {
     return days <= 365;
   });
 
-  return (
-    <>
-      <h1>Expenses</h1>
+  const filteredExpenses = (tab) => {
+    switch (tab) {
+      case "week":
+        return selectedCategory === "all"
+          ? weeklyExpenses
+          : weeklyExpenses.filter(
+              (expense) => expense.category === selectedCategory,
+            );
+      case "month":
+        return selectedCategory === "all"
+          ? monthlyExpenses
+          : monthlyExpenses.filter(
+              (expense) => expense.category === selectedCategory,
+            );
+      case "year":
+        return selectedCategory === "all"
+          ? yearlyExpenses
+          : yearlyExpenses.filter(
+              (expense) => expense.category === selectedCategory,
+            );
+      default:
+        return [];
+    }
+  };
 
-      <Tabs
-        value={activeTab}
-        onValueChange={setActiveTab}
-        className="w-[400px]"
-      >
-        <TabsList className="w-[800px]">
-          <TabsTrigger value="Week">Week</TabsTrigger>
-          <TabsTrigger value="Month">Month</TabsTrigger>
-          <TabsTrigger value="Year">Year</TabsTrigger>
+  return (
+    <main className="m-24 w-[800px]">
+      <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
+        <TabsList className="w-full">
+          <TabsTrigger className="w-full" value="Week">
+            Week
+          </TabsTrigger>
+          <TabsTrigger className="w-full" value="Month">
+            Month
+          </TabsTrigger>
+          <TabsTrigger className="w-full" value="Year">
+            Year
+          </TabsTrigger>
         </TabsList>
         <div className="mb-6">
           <Label htmlFor="category-select" className="mb-2">
@@ -78,22 +107,23 @@ const ExpenseTable = (props) => {
             id="category-select"
             value={selectedCategory}
             onValueChange={setSelectedCategory}
-            className="w-full max-w-[200px]"
           >
-            <SelectTrigger>
+            <SelectTrigger className="w-48">
               <SelectValue placeholder="All" />
             </SelectTrigger>
             <SelectContent>
               <SelectItem value="all">All</SelectItem>
-              <SelectItem value="Household">Household</SelectItem>
-              <SelectItem value="Food">Food</SelectItem>
-              <SelectItem value="Utilities">Utilities</SelectItem>
-              <SelectItem value="Housing">Housing</SelectItem>
-              <SelectItem value="Transportation">Transportation</SelectItem>
-              <SelectItem value="Entertainment">Entertainment</SelectItem>
-              <SelectItem value="Travel">Travel</SelectItem>
-              <SelectItem value="Health">Health</SelectItem>
-              <SelectItem value="Gifts">Gifts</SelectItem>
+              <SelectItem value="food_groceries">Food</SelectItem>
+              <SelectItem value="transportation">Transportation</SelectItem>
+              <SelectItem value="housing">Housing</SelectItem>
+              <SelectItem value="other">Other</SelectItem>
+              <SelectItem value="entertainment">Entertainment</SelectItem>
+              <SelectItem value="clothing">Clothing</SelectItem>
+              <SelectItem value="utilities">Utilities</SelectItem>
+              <SelectItem value="medical">Medical</SelectItem>
+              <SelectItem value="insurance">Insurance</SelectItem>
+              <SelectItem value="personal">Personal</SelectItem>
+              <SelectItem value="education">Education</SelectItem>
             </SelectContent>
           </Select>
         </div>
@@ -114,12 +144,12 @@ const ExpenseTable = (props) => {
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {weekExpenses.map((expense) => {
+                  {filteredExpenses("week").map((expense) => {
                     return (
                       <TableRow key={expense.id}>
                         <TableCell>{expense.name}</TableCell>
-                        <TableCell>{expense.amount}</TableCell>
-                        <TableCell>{expense.date}</TableCell>
+                        <TableCell>${expense.amount}</TableCell>
+                        <TableCell>{formatDate(expense.date)}</TableCell>
                         <TableCell>{expense.category}</TableCell>
                       </TableRow>
                     );
@@ -146,12 +176,12 @@ const ExpenseTable = (props) => {
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {monthExpenses.map((expense) => {
+                  {filteredExpenses("month").map((expense) => {
                     return (
                       <TableRow key={expense.id}>
                         <TableCell>{expense.name}</TableCell>
                         <TableCell>{expense.amount}</TableCell>
-                        <TableCell>{expense.date}</TableCell>
+                        <TableCell>{formatDate(expense.date)}</TableCell>
                         <TableCell>{expense.category}</TableCell>
                       </TableRow>
                     );
@@ -178,12 +208,12 @@ const ExpenseTable = (props) => {
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {yearExpenses.map((expense) => {
+                  {filteredExpenses("year").map((expense) => {
                     return (
                       <TableRow key={expense.id}>
                         <TableCell>{expense.name}</TableCell>
                         <TableCell>{expense.amount}</TableCell>
-                        <TableCell>{expense.date}</TableCell>
+                        <TableCell>{formatDate(expense.date)}</TableCell>
                         <TableCell>{expense.category}</TableCell>
                       </TableRow>
                     );
@@ -194,7 +224,7 @@ const ExpenseTable = (props) => {
           </Card>
         </TabsContent>
       </Tabs>
-    </>
+    </main>
   );
 };
 
