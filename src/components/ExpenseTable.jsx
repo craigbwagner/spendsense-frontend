1e1a20f07073e2c3bf61e0ac435c4f2850e926ee
menachemlemmer <nmmlemmer@gmail.com>
2024-08-02 17:46:19 -0400

    Add scroll behavior

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -32,6 +32,7 @@ import { Label } from "./ui/label";
 const ExpenseTable = (props) => {
   const [activeTab, setActiveTab] = useState("Week");
   const [selectedCategory, setSelectedCategory] = useState("all");
+
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "short", day: "2-digit" };
     return new Date(dateString).toLocaleDateString(undefined, options);
@@ -110,205 +111,72 @@ const ExpenseTable = (props) => {
           </TabsTrigger>
         </TabsList>
 
-        <TabsContent value="Week">
-          <Card className="w-[800px]">
-            <CardHeader className="flex flex-row items-start justify-between pb-8">
-              <div>
-                <CardTitle>This week's expenses</CardTitle>
-                <CardDescription>All expenses for this week </CardDescription>
-              </div>
-              <div className="m-0 flex items-center gap-4 p-0">
-                <Label htmlFor="category-select">Category:</Label>
-                <Select
-                  id="category-select"
-                  value={selectedCategory}
-                  onValueChange={setSelectedCategory}
-                >
-                  <SelectTrigger className="m-0 w-48">
-                    <SelectValue placeholder="All" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="all">All</SelectItem>
-                    <SelectItem value="food_groceries">Food</SelectItem>
-                    <SelectItem value="transportation">
-                      Transportation
-                    </SelectItem>
-                    <SelectItem value="housing">Housing</SelectItem>
-                    <SelectItem value="other">Other</SelectItem>
-                    <SelectItem value="entertainment">Entertainment</SelectItem>
-                    <SelectItem value="clothing">Clothing</SelectItem>
-                    <SelectItem value="utilities">Utilities</SelectItem>
-                    <SelectItem value="medical">Medical</SelectItem>
-                    <SelectItem value="insurance">Insurance</SelectItem>
-                    <SelectItem value="personal">Personal</SelectItem>
-                    <SelectItem value="education">Education</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Amount</TableHead>
-                    <TableHead>Date</TableHead>
-                    <TableHead>Category</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {filteredExpenses("week").map((expense) => {
-                    return (
-                      <TableRow key={expense.id}>
-                        <TableCell>{expense.name}</TableCell>
-                        <TableCell>${expense.amount}</TableCell>
-                        <TableCell>{formatDate(expense.date)}</TableCell>
-                        <TableCell>
-                          {expense.category == "food_groceries"
-                            ? "food"
-                            : expense.category}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </TabsContent>
-        <TabsContent value="Month">
-          <Card className="w-[800px]">
-            <CardHeader className="flex flex-row items-start justify-between pb-8">
-              <div>
-                <CardTitle>This month's expenses</CardTitle>
-                <CardDescription>All expenses for this month</CardDescription>
-              </div>
-              <div className="m-0 flex items-center gap-4 p-0">
-                <Label htmlFor="category-select">Category:</Label>
-                <Select
-                  id="category-select"
-                  value={selectedCategory}
-                  onValueChange={setSelectedCategory}
-                >
-                  <SelectTrigger className="w-48">
-                    <SelectValue placeholder="All" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="all">All</SelectItem>
-                    <SelectItem value="food_groceries">Food</SelectItem>
-                    <SelectItem value="transportation">
-                      Transportation
-                    </SelectItem>
-                    <SelectItem value="housing">Housing</SelectItem>
-                    <SelectItem value="other">Other</SelectItem>
-                    <SelectItem value="entertainment">Entertainment</SelectItem>
-                    <SelectItem value="clothing">Clothing</SelectItem>
-                    <SelectItem value="utilities">Utilities</SelectItem>
-                    <SelectItem value="medical">Medical</SelectItem>
-                    <SelectItem value="insurance">Insurance</SelectItem>
-                    <SelectItem value="personal">Personal</SelectItem>
-                    <SelectItem value="education">Education</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Amount</TableHead>
-                    <TableHead>Date</TableHead>
-                    <TableHead>Category</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {filteredExpenses("month").map((expense) => {
-                    return (
-                      <TableRow key={expense.id}>
-                        <TableCell>{expense.name}</TableCell>
-                        <TableCell>${expense.amount}</TableCell>
-                        <TableCell>{formatDate(expense.date)}</TableCell>
-                        <TableCell>
-                          {expense.category == "food_groceries"
-                            ? "food"
-                            : expense.category}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </TabsContent>
-        <TabsContent value="Year">
-          <Card className="w-[800px]">
-            <CardHeader className="flex flex-row items-start justify-between pb-8">
-              <div>
-                <CardTitle>This year's expenses</CardTitle>
-                <CardDescription>All expenses for this year</CardDescription>
-              </div>
-              <div className="m-0 flex items-center gap-4 p-0">
-                <Label htmlFor="category-select">Category:</Label>
-                <Select
-                  id="category-select"
-                  value={selectedCategory}
-                  onValueChange={setSelectedCategory}
-                >
-                  <SelectTrigger className="w-48">
-                    <SelectValue placeholder="All" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="all">All</SelectItem>
-                    <SelectItem value="food_groceries">Food</SelectItem>
-                    <SelectItem value="transportation">
-                      Transportation
-                    </SelectItem>
-                    <SelectItem value="housing">Housing</SelectItem>
-                    <SelectItem value="other">Other</SelectItem>
-                    <SelectItem value="entertainment">Entertainment</SelectItem>
-                    <SelectItem value="clothing">Clothing</SelectItem>
-                    <SelectItem value="utilities">Utilities</SelectItem>
-                    <SelectItem value="medical">Medical</SelectItem>
-                    <SelectItem value="insurance">Insurance</SelectItem>
-                    <SelectItem value="personal">Personal</SelectItem>
-                    <SelectItem value="education">Education</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Amount</TableHead>
-                    <TableHead>Date</TableHead>
-                    <TableHead>Category</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {filteredExpenses("year").map((expense) => {
-                    return (
-                      <TableRow key={expense.id}>
-                        <TableCell>{expense.name}</TableCell>
-                        <TableCell>${expense.amount}</TableCell>
-                        <TableCell>{formatDate(expense.date)}</TableCell>
-                        <TableCell>
-                          {expense.category == "food_groceries"
-                            ? "food"
-                            : expense.category}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </TabsContent>
+        <TabsContent value="Week"></TabsContent>
+        <TabsContent value="Month"></TabsContent>
+        <TabsContent value="Year"></TabsContent>
       </Tabs>
+      <Card className="max-h-[600px] w-[800px] overflow-auto shadow-md">
+        <CardHeader className="flex flex-row items-start justify-between pb-8">
+          <div>
+            <CardTitle>This {activeTab}'s expenses</CardTitle>
+            <CardDescription>All expenses for this week </CardDescription>
+          </div>
+          <div className="m-0 flex items-center gap-4 p-0">
+            <Label htmlFor="category-select">Category:</Label>
+            <Select
+              id="category-select"
+              value={selectedCategory}
+              onValueChange={setSelectedCategory}
+            >
+              <SelectTrigger className="m-0 w-48">
+                <SelectValue placeholder="All" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="food_groceries">Food</SelectItem>
+                <SelectItem value="transportation">Transportation</SelectItem>
+                <SelectItem value="housing">Housing</SelectItem>
+                <SelectItem value="other">Other</SelectItem>
+                <SelectItem value="entertainment">Entertainment</SelectItem>
+                <SelectItem value="clothing">Clothing</SelectItem>
+                <SelectItem value="utilities">Utilities</SelectItem>
+                <SelectItem value="medical">Medical</SelectItem>
+                <SelectItem value="insurance">Insurance</SelectItem>
+                <SelectItem value="personal">Personal</SelectItem>
+                <SelectItem value="education">Education</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Amount</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead>Category</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {filteredExpenses(activeTab.toLowerCase()).map((expense) => {
+                return (
+                  <TableRow key={expense.id}>
+                    <TableCell>{expense.name}</TableCell>
+                    <TableCell>${expense.amount}</TableCell>
+                    <TableCell>{formatDate(expense.date)}</TableCell>
+                    <TableCell>
+                      {expense.category == "food_groceries"
+                        ? "food"
+                        : expense.category}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
     </main>
   );
 };
