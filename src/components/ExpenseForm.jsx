6eab694e9806ef7817a725acf411c844b5416e31
menachemlemmer <nmmlemmer@gmail.com>
2024-08-01 22:22:55 -0400

    Create expense form

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -11,20 +10,38 @@ import {
   FormLabel,
   FormMessage,
 } from "./ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import * as expensesService from "../services/expensesService";
 
 const formSchema = z.object({
   name: z.string().min(5).max(40),
-  amount: z.number().min(0.01),
-  date: z.date(),
+  amount: z.coerce.number().positive().min(0.01),
+  date: z.coerce.date(),
+  category: z.enum([
+    "food",
+    "transportation",
+    "housing",
+    "other",
+    "entertainment",
+    "clothing",
+    "utilities",
+    "medical",
+    "insurance",
+    "personal",
+    "education",
+  ]),
 });
 
-const ExpenseForm = (props) => {
+const ExpenseForm = () => {
   const navigate = useNavigate();
-
-  const expense = props.expense;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -36,13 +53,10 @@ const ExpenseForm = (props) => {
   });
 
   const handleSubmit = async (data) => {
+    console.log(data);
     try {
-      if (expense.id) {
-        await expensesService.update(expense.id, data);
-      } else {
-        await expensesService.create(data);
-      }
-
+      const createdExpense = await expensesService.create(data);
+      console.log(createdExpense);
       navigate("/");
     } catch (err) {
       console.log(err);
@@ -54,7 +68,9 @@ const ExpenseForm = (props) => {
     <main className="m-8 flex h-full w-full flex-col items-center justify-center">
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit(handleSubmit)}
+          onSubmit={form.handleSubmit((data) => {
+            handleSubmit(data);
+          })}
           className="flex w-full max-w-md flex-col gap-4"
         >
           <FormField
@@ -87,6 +103,59 @@ const ExpenseForm = (props) => {
               );
             }}
           />
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Date</FormLabel>
+                  <FormControl>
+                    <Input type="date" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name="category"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="category" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="food">Food</SelectItem>
+                      <SelectItem value="transportation">
+                        Transportation
+                      </SelectItem>
+                      <SelectItem value="housing">Housing</SelectItem>
+                      <SelectItem value="other">Other</SelectItem>
+                      <SelectItem value="entertainment">
+                        Entertainment
+                      </SelectItem>
+                      <SelectItem value="clothing">Clothing</SelectItem>
+                      <SelectItem value="utilities">Utilities</SelectItem>
+                      <SelectItem value="medical">Medical</SelectItem>
+                      <SelectItem value="insurance">Insurance</SelectItem>
+                      <SelectItem value="personal">Personal</SelectItem>
+                      <SelectItem value="education">Education</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
           <Button type="submit">Submit</Button>
         </form>
       </Form>
