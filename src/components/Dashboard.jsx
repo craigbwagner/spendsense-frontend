dd2bceb5ccdb6ea1947469a9cd5f45ff95c0839e
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-06 13:01:39 -0400

    move pie chart out of expense form card

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -17,14 +17,12 @@ const Dashboard = (props) => {
       <Card className="m-24 max-h-[600px] w-fit min-w-[400px] max-w-[800px] shadow-md">
         <CardHeader>
           <CardTitle>Add Expense</CardTitle>
-          <CardDescription></CardDescription>
         </CardHeader>
-
         <CardContent>
           <ExpenseForm handleCreateExpense={props.handleCreateExpense} />
         </CardContent>
-        <CategoriesPieChart expenses={props.expenses} />
       </Card>
+      <CategoriesPieChart expenses={props.expenses} />
     </div>
   );
 };
