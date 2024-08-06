57a197f9573ed8962131995969ad2bc57236c254
menachemlemmer <nmmlemmer@gmail.com>
2024-08-06 16:06:57 -0400

    Add dashboard media query

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,7 +6,7 @@ import CategoriesPieChart from "./CategoriesPieChart";
 
 const Dashboard = (props) => {
   return (
-    <main className="grid grid-cols-2 gap-16 px-24 py-12">
+    <main className="grid grid-cols-1 gap-16 px-24 py-12 lg:grid-cols-2">
       <div className="grid gap-8">
         <ExpenseTable {...props} />
         <Card className="max-h-[600px] w-fit min-w-[400px] max-w-[800px] shadow-md">
