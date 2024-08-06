940aed914f723e8f01c8de6396ef90c1f40ec178
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-06 14:22:08 -0400

    add settings card to spending page

--- src/components/Spending.jsx
+++ src/components/Spending.jsx
@@ -17,7 +17,25 @@ const Dashboard = (props) => {
         <CardContent>
           <CardDescription>
             <div className="flex flex-col gap-2">
-              <h6 className="text-lg font-semibold">Monthly Budget</h6>
+                <h6 className="font-semibold">Income: ${props.settings.income}</h6>
+                <h6 className="font-semibold">Budget: ${props.settings.budget}</h6>
+                <h6 className="font-semibold">Savings Goal: ${props.settings.savingsGoal}</h6>
+              <button className="bg-blue-500 text-white">Update Settings</button>
+              <div className="grid grid-columns-2">
+                <h3 className="text-xl m-2">Budgets by Category</h3>
+                <h6 className="font-semibold">Housing: ${props.categoryBudgets.housing}</h6>
+                <h6 className="font-semibold">Transportation: ${props.categoryBudgets.transportation}</h6>
+                <h6 className="font-semibold">Food/Groceries: ${props.categoryBudgets.food_groceries}</h6>
+                <h6 className="font-semibold">Utilities: ${props.categoryBudgets.utilities}</h6>
+                <h6 className="font-semibold">Clothing: ${props.categoryBudgets.clothing}</h6>
+                <h6 className="font-semibold">Medical: ${props.categoryBudgets.medical}</h6>
+                <h6 className="font-semibold">Insurance: ${props.categoryBudgets.insurance}</h6>
+                <h6 className="font-semibold">Personal: ${props.categoryBudgets.personal}</h6>
+                <h6 className="font-semibold">Education: ${props.categoryBudgets.education}</h6>
+                <h6 className="font-semibold">Entertainment: ${props.categoryBudgets.entertainment}</h6>
+                <h6 className="font-semibold">Other: ${props.categoryBudgets.other}</h6>
+                <button className="bg-blue-500 text-white">Update Budgets</button>
+              </div>
             </div>
           </CardDescription>
         </CardContent>
