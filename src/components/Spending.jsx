65a951589a94fd10decc41a20b809cbec057bda9
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-06 14:36:07 -0400

    add margins to settings card

--- src/components/Spending.jsx
+++ src/components/Spending.jsx
@@ -6,23 +6,27 @@ import {
   CardTitle,
   CardDescription,
 } from "./ui/card";
+import { Button } from "./ui/button";
 
 const Dashboard = (props) => {
   return (
     <div>
-      <Card className="m-24 max-h-[600px] w-fit min-w-[400px] max-w-[800px] shadow-md">
+      <Card className="m-24 max-h-[600px] w-fit min-w-[500px] max-w-[800px] shadow-md">
         <CardHeader>
           <CardTitle>Configured Settings</CardTitle>
         </CardHeader>
         <CardContent>
           <CardDescription>
-            <div className="flex flex-col gap-2">
-                <h6 className="font-semibold">Income: ${props.settings.income}</h6>
-                <h6 className="font-semibold">Budget: ${props.settings.budget}</h6>
-                <h6 className="font-semibold">Savings Goal: ${props.settings.savingsGoal}</h6>
-              <button className="bg-blue-500 text-white">Update Settings</button>
-              <div className="grid grid-columns-2">
-                <h3 className="text-xl m-2">Budgets by Category</h3>
+            <div className="flex flex-col gap-3">
+              <h3 className="text-xl mb-3">User Settings</h3>
+              <div className="grid grid-cols-2 gap-4 mb-3">
+                <h6 className="font-semibold">Monthly Income: ${props.settings.income}</h6>
+                <h6 className="font-semibold">Monthly Budget: ${props.settings.budget}</h6>
+                <h6 className="font-semibold">Monthly Savings Goal: ${props.settings.savingsGoal}</h6>
+              </div>
+              <Button className="bg-blue-500 text-white">Update Settings</Button>
+              <h3 className="text-xl mb-3">Budgets by Category</h3>
+              <div className="grid grid-cols-2 gap-4 mb-3">
                 <h6 className="font-semibold">Housing: ${props.categoryBudgets.housing}</h6>
                 <h6 className="font-semibold">Transportation: ${props.categoryBudgets.transportation}</h6>
                 <h6 className="font-semibold">Food/Groceries: ${props.categoryBudgets.food_groceries}</h6>
@@ -34,8 +38,8 @@ const Dashboard = (props) => {
                 <h6 className="font-semibold">Education: ${props.categoryBudgets.education}</h6>
                 <h6 className="font-semibold">Entertainment: ${props.categoryBudgets.entertainment}</h6>
                 <h6 className="font-semibold">Other: ${props.categoryBudgets.other}</h6>
-                <button className="bg-blue-500 text-white">Update Budgets</button>
               </div>
+              <Button className="bg-blue-500 text-white">Update Budgets</Button>
             </div>
           </CardDescription>
         </CardContent>
